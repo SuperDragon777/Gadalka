6941b1b3bb41fb9a5764abce74d0f9b9a13979ba
Mglumov <matveyglumovn@gmail.com>
2026-01-28 15:22:29 +0300

    Попытки

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import random
+
 vibor_bota = random.randint(1, 10)
+trys = 5
+
 while True:
-    vibor_chela = int(input())
+    if not trys == 0:
+        print(f"Ваши попытки: {trys}")
+    else:
+        print("Кончились попытки, лох")
+        break
+        
+    try:
+        vibor_chela = int(input("🌚Выбери число🌚 "))
+    except:
+        print("Инвалид")
+        break
+        
     if vibor_chela == vibor_bota:
         print("Попеда еблан")
         vibor_bota = random.randint(1, 10)
+        trys = 5
     else:
+        trys -= 1
         if vibor_chela > vibor_bota:
             print("Меньше")
         else:
